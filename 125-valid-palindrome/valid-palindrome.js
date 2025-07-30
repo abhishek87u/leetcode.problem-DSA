e8fd48fe1f08dbafe031c44ebe7a8ec171101c70
ꪖ᥇ꫝ꠸ᦓꫝꫀᛕ ρꪖꪀᦔꫀꪗ <abhishek4795pandey@gmail.com>
2025-07-30 10:45:33 +0530

    Time: 4 ms (76.57%) | Memory: 57.8 MB (60.65%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+    let cleaned = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+    
+    let reversed = cleaned.split('').reverse().join('');
+    return cleaned === reversed;
+};
