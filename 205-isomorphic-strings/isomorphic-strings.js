36c5c20696375a5d7d946aa9f68bd1cf9a84c794
ꪖ᥇ꫝ꠸ᦓꫝꫀᛕ ρꪖꪀᦔꫀꪗ <abhishek4795pandey@gmail.com>
2025-07-30 10:49:03 +0530

    Time: 6 ms (66.88%) | Memory: 56.6 MB (20.52%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    let mapST = {};
+    let mapTS = {};
+
+    for (let i = 0; i < s.length; i++) {
+        let charS = s[i];
+        let charT = t[i];
+
+        if ((mapST[charS] && mapST[charS] !== charT) ||
+            (mapTS[charT] && mapTS[charT] !== charS)) {
+            return false;
+        }
+
+        mapST[charS] = charT;
+        mapTS[charT] = charS;
+    }
+
+    return true;
+};
